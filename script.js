a139c16254a654482b106b4dbdffaed3b259a61b
Lucas Antunes de Lara <lucasantuneslara06@gmail.com>
2022-07-19 19:49:35 -0300

    Adicionei contador

--- script.js
+++ script.js
@@ -1,26 +1,65 @@
-var countDownDate = new Date("Nov 21, 2022 07:00:00").getTime();
+// Data a ser contada
+var countDownDate = new Date("Jan 5, 2024 15:37:25").getTime();
 
-// Update the count down every 1 second
+// Atualiza o contador a cada 1 segundo
 var x = setInterval(function() {
+
+  // Data de hoje
+  var agora = new Date().getTime();
     
-  // Get today's date and time
-  var now = new Date().getTime();
+  // Calcula a distancia entre hoje e a data final
+  var distancia = countDownDate - agora;
+    
+  // Cálculo dos dias, horas, minutos e segundos
+  var dias = Math.floor(distancia / (1000 * 60 * 60 * 24));
+  var horas = Math.floor((distancia % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  var minutos = Math.floor((distancia % (1000 * 60 * 60)) / (1000 * 60));
+  var segundos = Math.floor((distancia % (1000 * 60)) / 1000);
+    
+  // Ele mosta o resultado na #demo
+  document.getElementById("demo").innerHTML = "A copa começa em: " + dias + "d " + horas + "h "
+  + minutos + "m " + segundos + "s ";
+    
 
-  // Find the distance between now and the count down date
-  var distance = countDownDate - now;
-
-  // Time calculations for days, hours, minutes and seconds
-  var days = Math.floor(distance / (1000 * 60 * 60 * 24));
-  var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-  var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-  var seconds = Math.floor((distance % (1000 * 60)) / 1000);
-
-  // Display the result in the element with id="demo"
-  document.getElementById("data").innerHTML = days + "d " + hours + "h "
-  + minutes + "m " + seconds + "s ";
-
-  // If the count down is finished, write some text
-  if (distance < 0) {
+  // O que mostrar quando o contador acabar
+  if (distancia < 0) {
     clearInterval(x);
-    document.getElementById("data").innerHTML = "A copa já começou";
-}
+    // document.getElementById("demo").innerHTML = "EXPIRED";
+    alert("Ei, copa já começou!")
+  }
+}, 1000);
+
+//Navbar
+
+document.addEventListener('DOMContentLoaded', () => {
+
+  // Get all "navbar-burger" elements
+  const $navbarBurgers = Array.prototype.slice.call(document.querySelectorAll('.navbar-burger'), 0);
+
+  // Add a click event on each of them
+  $navbarBurgers.forEach( el => {
+    el.addEventListener('click', () => {
+
+      // Get the target from the "data-target" attribute
+      const target = el.dataset.target;
+      const $target = document.getElementById(target);
+
+      // Toggle the "is-active" class on both the "navbar-burger" and the "navbar-menu"
+      el.classList.toggle('is-active');
+      $target.classList.toggle('is-active');
+
+    });
+  });
+
+});
+$(document).ready(function() {
+
+  // Check for click events on the navbar burger icon
+  $(".navbar-burger").click(function() {
+
+      // Toggle the "is-active" class on both the "navbar-burger" and the "navbar-menu"
+      $(".navbar-burger").toggleClass("is-active");
+      $(".navbar-menu").toggleClass("is-active");
+
+  });
+});
