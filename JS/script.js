44b99b9930e80182f1aa4c0e7dd8a6dd95ffc9eb
Lucas Antunes de Lara <lucasantuneslara06@gmail.com>
2022-08-17 14:30:11 -0300

    Mais mudanças no design

--- JS/script.js
+++ JS/script.js
@@ -1,5 +1,5 @@
 // Data a ser contada
-var countDownDate = new Date("Nov 20, 2022 15:00:00").getTime();
+var countDownDate = new Date("Nov 20, 2022 13:00:00").getTime();
 
 // Atualiza o contador a cada 1 segundo
 var x = setInterval(function() {
@@ -15,17 +15,23 @@ var x = setInterval(function() {
   var horas = Math.floor((distancia % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
   var minutos = Math.floor((distancia % (1000 * 60 * 60)) / (1000 * 60));
   var segundos = Math.floor((distancia % (1000 * 60)) / 1000);
+  const d = "DIAS"
+  const h = "HORAS"
+  const m = "MINUTOS"
+  const s = "SEGUNDOS"
+  
+  
     
   // Ele mosta o resultado na #demo
   //document.getElementById("demo").innerHTML = "A copa começa em: " + dias + "d " + horas + "h "
   //+ minutos + "m " + segundos + "s ";
-  document.getElementById("days").innerHTML =  " " + dias + "d"
+  document.getElementById("days").innerHTML =  " " + dias + "<h6><br>" + d + "</h6>";
 
-  document.getElementById("hours").innerHTML = horas + "h"
+  document.getElementById("hours").innerHTML = horas + "<h6><br>" + h + "</h6>";
 
-  document.getElementById("minutes").innerHTML = minutos + "m"
+  document.getElementById("minutes").innerHTML = minutos + "<h6><br>" + m + "</h6>";
 
-  document.getElementById("seconds").innerHTML = segundos + "s"
+  document.getElementById("seconds").innerHTML = segundos + "<h6><br>" + s + "</h6>"
 
   // O que mostrar quando o contador acabar
   if (distancia < 0) {
@@ -41,36 +47,28 @@ function mudarFundo(){
   const td = document.getElementsByClassName('tabela')
   const img = document.getElementsByClassName('hover')
   const body = document.body;
-  const icon = document.getElementsByClassName('material-symbols-rounded')
-  const icon2 = document.getElementsByClassName('carousel-control-prev-icon')
+  // const icon = document.getElementsByClassName('material-symbols-rounded')
+  // const Icon = document.getElementById('icn')
+  // const icon2 = document.getElementsByClassName('carousel-control-prev-icon')
   const infoEstadio = document.getElementsByClassName('infos')
   body.classList.toggle("lightBG");
-  tabela.classList.toggle("tabelaDark");
-  icon.classList.toggle('lightBG');
-  icon2.classList.toggle('lightBG');
-  img.classList.toggle('lightBG')
-  infoEstadio.classList.toggle('infosLight')
-  
-  const fundo = document.getElementById('carouselExampleFade2')
-  const back = document.getElementById('carouselExampleFade')
-  fundo.style.display = "none"
-  if(fundo === none){
-    fundo.style.display = "block"
-    back.style.display = "none"
-  }
-  else{
-    fundo.style.display = "none"
-    back.style.display = "block"
-  }
-}
 
-function Img(){
-  var A = document.getElementById('A').src = 'Imgs/Grupos Light/Grupo A.png'
-  var B = document.getElementById('B').src = 'Imgs/Grupos Light/Grupo B.png'
-  var C = document.getElementById('C').src = 'Imgs/Grupos Light/Grupo C.png'
-  var D = document.getElementById('D').src = 'Imgs/Grupos Light/Grupo D.png'
-  var E = document.getElementById('E').src = 'Imgs/Grupos Light/Grupo E.png'
-  var F = document.getElementById('F').src = 'Imgs/Grupos Light/Grupo F.png'
-  var G = document.getElementById('G').src = 'Imgs/Grupos Light/Grupo G.png'
-  var H = document.getElementById('H').src = 'Imgs/Grupos Light/Grupo H.png'
+  var x = document.getElementById("icn");
+  if (x.innerHTML === "light_mode") {
+    x.innerHTML = "dark_mode";
+    x.style.transition = 'all 1s linear'
+  } else {
+    x.innerHTML = "light_mode";
+    x.style.transition = 'all 1s linear'
+  }
+  console.log(x)
+  // tabela.classList.toggle("tabelaDark");
+  //icon.classList.toggle('lightBG');
+ // icon2.classList.toggle('lightBG');
+ // img.classList.toggle('lightBG')
+  //infoEstadio.classList.toggle('infosLight')
+
+
+  //const fundo = document.getElementById('carouselExampleFade2')
+  //const back = document.getElementById('carouselExampleFade')
 }
