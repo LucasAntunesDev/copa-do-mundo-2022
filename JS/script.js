84967c7ef352365425e45d41834b2a91759c0695
Lucas Antunes de Lara <lucasantuneslara06@gmail.com>
2022-10-10 19:46:13 -0300

    nav-bar responiva

--- JS/script.js
+++ JS/script.js
@@ -1,16 +1,15 @@
-// Data a ser contada
+function $(id) {
+  return document.getElementById(id);
+}
+
 var countDownDate = new Date("Nov 20, 2022 13:00:00").getTime();
 
-// Atualiza o contador a cada 1 segundo
-var x = setInterval(function() {
+var x = setInterval(function () {
 
-  // Data de hoje
   var agora = new Date().getTime();
-    
-  // Calcula a distancia entre hoje e a data final
+
   var distancia = countDownDate - agora;
-    
-  // Cálculo dos dias, horas, minutos e segundos
+
   var dias = Math.floor(distancia / (1000 * 60 * 60 * 24));
   var horas = Math.floor((distancia % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
   var minutos = Math.floor((distancia % (1000 * 60 * 60)) / (1000 * 60));
@@ -19,56 +18,80 @@ var x = setInterval(function() {
   const h = "HORAS"
   const m = "MINUTOS"
   const s = "SEGUNDOS"
+
+  $("days").innerHTML = " " + dias + "<h6><br>" + d + "</h6>";
+
+  $("hours").innerHTML = horas + "<h6><br>" + h + "</h6>";
+
+  $("minutes").innerHTML = minutos + "<h6><br>" + m + "</h6>";
+
+  $("seconds").innerHTML = segundos + "<h6><br>" + s + "</h6>"
+
   
-  
-    
-  // Ele mosta o resultado na #demo
-  //document.getElementById("demo").innerHTML = "A copa começa em: " + dias + "d " + horas + "h "
-  //+ minutos + "m " + segundos + "s ";
-  document.getElementById("days").innerHTML =  " " + dias + "<h6><br>" + d + "</h6>";
-
-  document.getElementById("hours").innerHTML = horas + "<h6><br>" + h + "</h6>";
-
-  document.getElementById("minutes").innerHTML = minutos + "<h6><br>" + m + "</h6>";
-
-  document.getElementById("seconds").innerHTML = segundos + "<h6><br>" + s + "</h6>"
-
-  // O que mostrar quando o contador acabar
   if (distancia < 0) {
     clearInterval(x);
-    // document.getElementById("demo").innerHTML = "EXPIRED";
-    alert("Ei, copa já começou!")
+    alert("A copa já começou!")
   }
 }, 1000);
 
-function mudarFundo(){
-  //const fundo = document.getElementById('corpo')
-  
+function mudarFundo() {
+
   const td = document.getElementsByClassName('tabela')
   const img = document.getElementsByClassName('hover')
   const body = document.body;
-  // const icon = document.getElementsByClassName('material-symbols-rounded')
-  // const Icon = document.getElementById('icn')
-  // const icon2 = document.getElementsByClassName('carousel-control-prev-icon')
   const infoEstadio = document.getElementsByClassName('infos')
   body.classList.toggle("lightBG");
 
-  var x = document.getElementById("icn");
-  if (x.innerHTML === "light_mode") {
-    x.innerHTML = "dark_mode";
-    // x.style.transition = 'all 1s linear'
+  var toggleModes = $("icn");
+  if (toggleModes.innerHTML === "light_mode") {
+    toggleModes.innerHTML = "dark_mode";
+    $('A').src = "Imgs/Grupos 2/Grupo A.png"
+    $('B').src = "Imgs/Grupos 2/Grupo B.png"
+    $('C').src = "Imgs/Grupos 2/Grupo C.png"
+    $('D').src = "Imgs/Grupos 2/Grupo D.png"
+    $('E').src = "Imgs/Grupos 2/Grupo E.png"
+    $('F').src = "Imgs/Grupos 2/Grupo F.png"
+    $('G').src = "Imgs/Grupos 2/Grupo G.png"
+    $('H').src = "Imgs/Grupos 2/Grupo H.png"
   } else {
-    x.innerHTML = "light_mode";
-    // x.style.transition = 'all 1s linear'
+    toggleModes.innerHTML = "light_mode";
+    $('A').src = "Imgs/Grupos/Grupo A.png"
+    $('B').src = "Imgs/Grupos/Grupo B.png"
+    $('C').src = "Imgs/Grupos/Grupo C.png"
+    $('D').src = "Imgs/Grupos/Grupo D.png"
+    $('E').src = "Imgs/Grupos/Grupo E.png"
+    $('F').src = "Imgs/Grupos/Grupo F.png"
+    $('G').src = "Imgs/Grupos/Grupo G.png"
+    $('H').src = "Imgs/Grupos/Grupo H.png"
   }
-  console.log(x)
-  // tabela.classList.toggle("tabelaDark");
-  //icon.classList.toggle('lightBG');
- // icon2.classList.toggle('lightBG');
- // img.classList.toggle('lightBG')
-  //infoEstadio.classList.toggle('infosLight')
-
-
-  //const fundo = document.getElementById('carouselExampleFade2')
-  //const back = document.getElementById('carouselExampleFade')
 }
+
+const btnMobile = document.getElementById('btn-mobile');
+const gruposUl = document.getElementsByClassName('grupos-mobile');
+
+function toggleMenu(event) {
+  if (event.type === 'touchstart') event.preventDefault();
+  const nav = document.getElementById('nav');
+  nav.classList.toggle('active');
+  const active = nav.classList.contains('active');
+  event.currentTarget.setAttribute('aria-expanded', active);
+  if (active) {
+    event.currentTarget.setAttribute('aria-label', 'Fechar Menu');
+  } else {
+    event.currentTarget.setAttribute('aria-label', 'Abrir Menu');
+  }
+}
+
+function mostrarGrupos() {
+  
+}
+//function mostrarGrupos(){
+ 
+  //gruposUl.style.display = "block"
+//}
+
+btnMobile.addEventListener('click', toggleMenu);
+btnMobile.addEventListener('touchstart', toggleMenu);
+
+btnMobile.addEventListener('click', toggleMenu);
+btnMobile.addEventListener('touchstart', toggleMenu);
--- README.md
+++ README.md
@@ -6,4 +6,4 @@ A proposta inicial do trabalho consistiu em:
 - Criar um index.html que redimensionasse o usuário para a página de cada grupo
 - Lá, há uma tabela de classificação e outra de partidas do grupo.
 
-Posteriormente, adicionei também CSS e JS (não era necessário, visto que o trabalho era só sobre HTML, mas fiz para deixar o site com uma cara mais profissional)
+Posteriormente, utilizei de outros conteúdos aprendidos ao longo do ano letivo, como CSS e JS. 
