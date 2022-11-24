fdddc5634efdab96cde2de60eaee9aacc9963b07
Lucas Antunes <104913618+LucasAntunes06@users.noreply.github.com>
2022-11-24 19:52:35 -0300

    update

---
+++ src/JS/Grupo D.js
@@ -0,0 +1,72 @@
+$ = (id) => {
+    return document.getElementById(id);
+}
+
+create = (create) => {
+    return document.createElement(create);
+}
+
+function $$(classe) {
+    return document.getElementsByClassName(classe);
+}
+
+const tabela = [
+    // {Time: 'Equador', Pontos: '3', Jogos: '1',  Vitórias: '1', Empates: '0', Derrotas: '0',	GP: '2', GC: '0', Saldo de Gols: 2, Aproveitamento: '100%',},
+    {Time: 'Equador', Pontos: '3', Jogos: '1',  Vitórias: '1', Empates: '0', Derrotas: '0',	GP: '2', GC: '0', Saldo: '2', Aproveitamento: '100%', Últimos: 'V', Imagem:'https://cdn.countryflags.com/thumbs/ecuador/flag-round-250.png'},
+    {Time: 'Holanda', Pontos: '3', Jogos: '1',  Vitórias: '1', Empates: '0', Derrotas: '0',	GP: '2', GC: '0', Saldo: '2', Aproveitamento: '100%', Últimos: 'V', Imagem:'https://cdn.countryflags.com/thumbs/netherlands/flag-round-250.png'},
+    {Time: 'Senegal', Pontos: '0', Jogos: '1',  Vitórias: '0', Empates: '0', Derrotas: '1',	GP: '0', GC: '1', Saldo: '-2', Aproveitamento: '0%', Últimos: 'D', Imagem:'https://cdn.countryflags.com/thumbs/senegal/flag-round-250.png'},
+    {Time: 'Catar', Pontos: '0', Jogos: '1',  Vitórias: '0', Empates: '0', Derrotas: '1',	GP: '0', GC: '2', Saldo: '-2', Aproveitamento: '0%', Últimos: 'D', Imagem:'https://cdn.countryflags.com/thumbs/qatar/flag-round-250.png'},
+];
+
+const montarTabela = () => {
+    //1 - Recuperar o elemnto HTML
+    const tbody = $('corpo');
+
+    for (let i = 0; i < tabela.length; i++) {
+        // creates a table row
+        const tr = create('tr');
+
+        for (let j = 0; j < 1; j++) {
+            const td1 = create('td');
+            td1.innerHTML = tabela[i].Time;
+            const td2 = create('td');
+            td2.innerHTML = tabela[i].Pontos;
+            const td3 = create('td');
+            td3.innerHTML = tabela[i].Jogos;
+            const td4 = create('td');
+            td4.innerHTML = tabela[i].Vitórias;
+            const td5 = create('td');
+            td5.innerHTML = tabela[i].Empates;
+            const td6 = create('td');
+            td6.innerHTML = tabela[i].Derrotas;
+            const td7 = create('td');
+            td7.innerHTML = tabela[i].GP;
+            const td8 = create('td');
+            td8.innerHTML = tabela[i].GC;
+            const td9 = create('td');
+            td9.innerHTML = tabela[i].Saldo;
+            const td10 = create('td');
+            td10.innerHTML = tabela[i].Aproveitamento;
+            const td11 = create('td');
+            td11.innerHTML = tabela[i].Últimos;
+            //Adicionando as td's como filhas
+            tr.appendChild(td1);
+            tr.appendChild(td2);
+            tr.appendChild(td3);
+            tr.appendChild(td4);
+            tr.appendChild(td5);
+            tr.appendChild(td6);
+            tr.appendChild(td7);
+            tr.appendChild(td8);
+            tr.appendChild(td9);
+            tr.appendChild(td10);
+            tr.appendChild(td11);
+            tr.setAttribute('class', 'hover:bg-pink-800');
+            tbody.appendChild(tr);
+
+        }
+    
+    }
+}
+
+montarTabela();
---
+++ tailwind.config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  content: ["src/**/*.{html,js}","html/**/*.{html,js}"],
+  theme: {
+  },
+  darkMode: 'class',
+  plugins: [],
+}
